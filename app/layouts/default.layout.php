3a67c4f42564b8dff1be82ad01c873cafeb5c032
devayes@gmail.com <devayes@gmail.com@4d040e7a-9b60-4065-5776-d069ed91c0b1>
2011-09-30 07:20:44 +0000

    moved some stuff around, prettied it up a little. very little

--- app/layouts/default.layout.php
+++ app/layouts/default.layout.php
@@ -10,7 +10,6 @@ if($headers = Registry::get('_SITE_HEADERS')){
 	}
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -67,61 +66,61 @@ if($headers = Registry::get('_SITE_HEADERS')){
     -->
   </head>
 
-  <body>
-
+<body>
     <div class="topbar">
-      <div class="fill">
-        <div class="container">
-          <a class="brand" href="#">Project name</a>
-          <ul class="nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
+        <div class="fill">
+            <div class="container">
+                <a class="brand" href="#">APE HMVC</a>
+                <ul class="nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
         </div>
-      </div>
     </div>
 
     <div class="container">
-
+    
       <!-- Main hero unit for a primary marketing message or call to action -->
-      <div class="hero-unit">
-        <h1>This is the default layout.</h1>
-          <p>You can have as many layouts as you want. Think of them as view containers. See the docs for more examples.</p>
-          <div id="test-content">
+        <div class="hero-unit">
+            <h1>APE HMVC Framework</h1>
+            <p>APE Framework provides a modular architecture, simple loading of libraries, helpers, plugins, regex style routing, flexible layout assignments and more.</p>
+            <p>What you get is a bare bones, fast, light, super flexible &amp; bloat free framework that works the way you want it to.</p>
+        </div>
+
+        <div class="row">
+            <div style="margin:0 20px 20px; width:100%;">
               <h2>View Data:</h2>
               <?php echo $view; ?>
               <em>From: /app/modules/home/views/default.view.php</em>
-              <hr />
               <h2>Controller "Simple Assignment" data:</h2>
               <p><?php echo $content; /* Alternatively Registry::get('main_content') */ ?></p>
               <em>From: /app/modules/home/controllers/default.controller.php</em>
-          </div>
-          
-          <p><em>/app/layouts/default.layout.php</em></p>
-      </div>
+            </div>
+        </div>
 
       <!-- Example row of columns -->
-      <div class="row">
-        <div class="span6">
-          <h2>Heading</h2>
-          <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
+        <div class="row">
+            <div class="span6">
+                <h2>Heading</h2>
+                <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
+            <div class="span5">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
+            <div class="span5">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
         </div>
-        <div class="span5">
-          <h2>Heading</h2>
-           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-       </div>
-        <div class="span5">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
 
       <footer>
-        <p>&copy; Company 2011 <!--debug--></p>
+        <p>&copy; Sitename 2011 <!--debug--></p>
       </footer>
 
     </div> <!-- /container -->
