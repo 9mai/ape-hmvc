b27188529bb97d7a46122dce7d536ca1258812ac
devayes@gmail.com <devayes@gmail.com@4d040e7a-9b60-4065-5776-d069ed91c0b1>
2011-11-14 03:34:51 +0000

    moving stuff, deprecating stuff

--- trunk/app/config/core.config.php
+++ trunk/app/config/core.config.php
@@ -7,16 +7,16 @@ $core['encryption_salt'] = 'xxxxxxxxxxxxx';
 
 
 // load config in loader class, check these, if anything, load em.
-$core['autoload']['helpers'] = array(
- /* 'common' loads: /helpers/common.helper.php */
+$core['autoload']['plugins'] = array(
+    /* 'common' loads: /plugins/common.plugin.php */
 );
 $core['autoload']['libraries'] = array(
     /*array('test' => array('foo'=>'bar', 'faz' => 'bax')) loads: libraries/test.php */
 );
-$core['autoload']['plugins'] = array();
 
 // hooks and autoload
 
+/* DEPRECATED for Router::getBaseURL() */
 $root = 'http://'.$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),'',$_SERVER['SCRIPT_NAME']);
 $core['base_url'] = $root;
